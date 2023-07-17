bf656c1c9ae9572d3e9848283dcc9c12386f93a0
Isekai Code <109307799+IsekaiCode@users.noreply.github.com>
2023-07-17 17:50:05 +0100

    Update and rename anime based on your mood.py to main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from random import choice, randint
 from bs4 import BeautifulSoup
 
 satisfied = "no"
-mood = input("are you happy/sad/bored/depressed/horny/hungry : ")
+mood = input("are you happy / sad / bored / depressed / horny / hungry : ")
+while mood not in ["happy", "sad", "depressed", "bored", "horny", "hungry"]:
+    mood = input("please select one of the listed keywords : ")
 
 recommandation = {
     "happy": choice((1, 2, 10, 22, 36, 10)),
@@ -14,12 +16,20 @@ recommandation = {
     "hungry": 47,
 }
 
-while satisfied == "no":
+def get_recommendation_list():
     r = requests.get(f"https://myanimelist.net/anime/genre/{str(recommandation[mood])}?page={randint(1,5)}")
     if r.status_code != 200:
         r = requests.get(f"https://myanimelist.net/anime/genre/{str(recommandation[mood])}?page=1")
     soup = BeautifulSoup(r.text, "lxml")
-    names = list(map(lambda name: name.text, list(soup.find_all("h2", {"class": "h2_anime_title"}))))
-    result = choice(names)
+    choices = list(map(lambda name: name.text, list(soup.find_all("h2", {"class": "h2_anime_title"}))))
+    return choices
+
+choices = get_recommendation_list()
+
+while satisfied == "no":
+    result = choice(choices)
     print(result)
+    choices.remove(result)
+    if len(choices) == 0:
+        choices = get_recommendation_list()
     satisfied = input("are you satisfied : ")
